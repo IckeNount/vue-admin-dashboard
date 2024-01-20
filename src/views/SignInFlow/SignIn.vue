a12baaa28bd1b5c24cd045dc1fc4f747414c7cc4
Aike Nount <aikenount@Aikes-MacBook-Air.local>
2024-01-21 02:07:21 +0630

    Create redirects file for Netlify

---
+++ src/views/SignInFlow/SignIn.vue
@@ -0,0 +1,96 @@
+<template lang="">
+  <div
+    class="container"
+    :class="{ 'light-background': !isDarkMode, 'dark-background': isDarkMode }"
+  >
+    <RequestAccount />
+    <div class="login">
+      <img class="bmw" src="@/assets/signin.svg" v-if="isDarkMode" />
+      <img class="bmw" src="@/assets/signin-light.svg" v-if="!isDarkMode" />
+      <h4 :class="{ 'light-text': isDarkMode, 'dark-text': !isDarkMode }">
+        Sign into Banana Molawi Wakanda
+      </h4>
+      <input
+        type="email"
+        placeholder="Email"
+        :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
+      />
+      <input
+        type="text"
+        placeholder="Password"
+        :class="{ 'light-field': isDarkMode, 'dark-field': !isDarkMode }"
+      />
+      <button>Sign In</button>
+      <router-link
+        :class="{ 'light-link': isDarkMode, 'dark-link': !isDarkMode }"
+        to="/recover"
+        >Forgot your password?</router-link
+      >
+      <ThemeSwitch />
+    </div>
+  </div>
+</template>
+
+<script>
+import RequestAccount from "@/components/RequestAccount.vue";
+import ThemeSwitch from "@/components/ThemeSwitch.vue";
+
+export default {
+  name: "SignIn",
+  components: {
+    RequestAccount,
+    ThemeSwitch,
+  },
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.bmw {
+  height: 200px;
+  width: 200px;
+  margin-bottom: 24px;
+}
+
+.login {
+  width: 400px;
+}
+
+h4 {
+  margin: 0;
+  line-height: 34px;
+  font-size: 24px;
+  text-align: center;
+  color: white;
+}
+
+input {
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  box-sizing: border-box;
+  border-radius: 4px;
+
+  height: 60px;
+  width: 100%;
+
+  font-size: 20px;
+  color: white;
+  padding-left: 20px;
+  margin-top: 20px;
+
+  &:placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+</style>
